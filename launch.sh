b847c73cdcf2b1dd9ba03181c879fcf94a8529f5
Batalha <felipebatalha96@gmail.com>
2023-04-11 15:25:26 -0300

    exibição da bateria

--- launch.sh
+++ launch.sh
@@ -7,7 +7,7 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" |
 polybar example 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
